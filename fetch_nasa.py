d75fb92cedbf3a69ddb53863edf9686061066f6a
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-08-19 18:44:33 +0300

    fixed the code

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 import pathlib
-from os.path import splitext
+from os.path import split
 from urllib.parse import urlsplit
 
 import requests
@@ -10,11 +10,60 @@ from dotenv import load_dotenv
 
 def get_extension(url):
     url_path = urlsplit(url).path
-    extension = splitext(url_path)[1]
+    extension = split(url_path)[1]
     return extension
 
 
-def fetch_nasa_last_launch(combined_filepath, download_url_response):
+def get_epic_links(url, token):
+    params = {'api_key': token}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    epic_links = response.json()
+    return (epic_links)
+
+
+def download_epic_images(epic_links, image_folder, token):
+    for link_number, link in enumerate(epic_links, 1):
+        image_name = link['image']
+        date = link['date']
+        date_time = datetime.datetime.fromisoformat(date)
+        formatted_date = date_time.strftime("%Y/%m/%d")
+        download_url = f'https://api.nasa.gov/EPIC/archive/natural/' \
+                       f'{formatted_date}/png/{image_name}.png'
+        extension = get_extension(download_url)
+        combined_filepath = os.path.join(
+            image_folder,
+            extension
+        )
+        download_image(combined_filepath, download_url, token)
+
+
+def get_apod_links(url, token, download_start_date):
+    params = {
+        'api_key': token,
+        'start_date': download_start_date
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    apod_links = response.json()
+    return (apod_links)
+
+
+def download_apod_images(apod_links, image_folder, token):
+    for link_number, link in enumerate(apod_links, 1):
+        image_url = link['url']
+        extension = get_extension(image_url)
+        combined_filepath = os.path.join(
+            image_folder,
+            extension
+        )
+        download_image(combined_filepath, image_url, token)
+
+
+def download_image(combined_filepath, download_url, token):
+    params = {'api_key': token}
+    download_url_response = requests.get(download_url, params=params)
+    download_url_response.raise_for_status()
     with open(combined_filepath, 'wb') as file:
         file.write(download_url_response.content)
 
@@ -22,31 +71,12 @@ def fetch_nasa_last_launch(combined_filepath, download_url_response):
 if __name__ == '__main__':
     load_dotenv()
     token = os.environ['SPACE_TOKEN']
-    filename = 'nasa'
-    folder_images = 'images'
-
-    url = f'https://api.nasa.gov/EPIC/api/natural/images?api_key={token}'
-    pathlib.Path(folder_images).mkdir(parents=True, exist_ok=True)
-    response = requests.get(url)
-    response.raise_for_status()
-    got_json = response.json()
-
-    for link_number, link in enumerate(got_json, 1):
-        params = {
-            'api_key': token
-        }
-        image_name = link['image']
-        date = link['date']
-        date_time = datetime.datetime.fromisoformat(date)
-        formatted_date = date_time.strftime("%Y/%m/%d")
-        download_url = f'https://api.nasa.gov/EPIC/archive/natural/' \
-            f'{formatted_date}/png/{image_name}.png'
-        download_url_response = requests.get(download_url, params=params)
-        download_url_response.raise_for_status()
-        image_url = download_url_response.url
-        extension = get_extension(image_url)
-        combined_filepath = os.path.join(
-            folder_images,
-            f'{filename}{link_number}{extension}'
-        )
-        fetch_nasa_last_launch(combined_filepath, download_url_response)
+    image_folder = 'images'
+    download_start_date = '2021-07-18'
+    epic_url = 'https://api.nasa.gov/EPIC/api/natural'
+    apod_url = 'https://api.nasa.gov/planetary/apod'
+    pathlib.Path(image_folder).mkdir(parents=True, exist_ok=True)
+    epic_links = get_epic_links(epic_url, token)
+    apod_links = get_apod_links(apod_url, token, download_start_date)
+    download_epic_images(epic_links, image_folder, token)
+    download_apod_images(apod_links, image_folder, token)
--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,47 +1,36 @@
 import os
 import pathlib
-from os.path import splitext
-from urllib.parse import urlsplit
 
 import requests
-from dotenv import load_dotenv
 
 
-def fetch_spacex_last_launch(combined_filepath, download_url_response):
+def get_spacex_links(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    spacex_links = response.json()['links']['flickr']['original']
+    return spacex_links
+
+
+def download_spacex_images(spacex_links, image_folder):
+    for link_number, link in enumerate(spacex_links, 1):
+        combined_filepath = os.path.join(
+            image_folder,
+            f'{filename}{link_number}.jpg'
+        )
+        download_image(combined_filepath, link)
+
+
+def download_image(combined_filepath, url):
+    response = requests.get(url)
+    response.raise_for_status()
     with open(combined_filepath, 'wb') as file:
-        file.write(download_url_response.content)
-
-
-def get_extension(url):
-    url_path = urlsplit(url).path
-    extension = splitext(url_path)[1]
-    return extension
+        file.write(response.content)
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    token = os.environ['SPACE_TOKEN']
     filename = 'spacex'
-    folder_images = 'images'
-    start_download_images = '2021-07-18'
-    url = 'https://api.nasa.gov/planetary/apod'
-    pathlib.Path(folder_images).mkdir(parents=True, exist_ok=True)
-
-    params = {
-        'api_key': token,
-        'start_date': start_download_images
-    }
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    got_json = response.json()
-
-    for link_number, link in enumerate(got_json, 1):
-        image_url = link['url']
-        extension = get_extension(image_url)
-        download_url_response = requests.get(image_url)
-        download_url_response.raise_for_status()
-        combined_filepath = os.path.join(
-            folder_images,
-            f'{filename}{link_number}{extension}'
-        )
-        fetch_spacex_last_launch(combined_filepath, download_url_response)
+    image_folder = 'images'
+    pathlib.Path(image_folder).mkdir(parents=True, exist_ok=True)
+    url = 'https://api.spacexdata.com/v4/launches/latest'
+    spacex_links = get_spacex_links(url)
+    download_spacex_images(image_folder, spacex_links)
